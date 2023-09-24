ee8d6ea30fd27ec8db8259b2b4db1b4753c2a556
mehm8128 <mehm8128@gmail.com>
2023-09-25 00:05:16 +0900

    tagHandlers入れ忘れ

--- src/lib/msw.ts
+++ src/lib/msw.ts
@@ -2,8 +2,9 @@ import { setupWorker } from 'msw'
 
 import { getApiOrigin } from './env'
 import { documentHandlers } from '@/clients/document/mocks'
+import { tagHandlers } from '@/clients/tag/mocks'
 
-const handlers = (apiOrigin: string) => [documentHandlers(apiOrigin)].flat()
+const handlers = (apiOrigin: string) => [documentHandlers(apiOrigin), tagHandlers(apiOrigin)].flat()
 
 export const initMock = () => {
   if (import.meta.env.MODE === 'development') {
