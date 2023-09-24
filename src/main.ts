58ff7fba510715306dfd1635b0a06a41155959b0
nokhnaton <sage_of_forest@icloud.com>
2023-09-24 19:29:58 +0900

    masonryレイアウトで表示されるように

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import mitt from 'mitt'
+
+import { VueMasonryPlugin } from 'vue-masonry'
+const emitter = mitt()
+
 const app = createApp(App)
+app.config.globalProperties.emitter = emitter
 
 app.use(createPinia())
 app.use(router)
+app.use(VueMasonryPlugin)
 
 app.mount('#app')
--- src/views/DocumentsView.vue
+++ src/views/DocumentsView.vue
@@ -1,28 +1,72 @@
 <script setup lang="ts">
 import DocumentCard from '@/components/Documents/DocumentCard.vue'
+
+const documents = [
+  {
+    imgSrc:
+      'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png',
+    username: 'noc7t',
+    title: 'ハンバーガー'
+  },
+  {
+    imgSrc: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+    username: 'nokhnaton',
+    title: 'よくわからん鳥'
+  },
+  {
+    imgSrc: 'https://placehold.jp/150x150.png',
+    username: 'noc7t',
+    title: '四角'
+  },
+  {
+    imgSrc: 'https://placehold.jp/150x300.png',
+    username: 'nokhnaton',
+    title: '四角'
+  },
+  {
+    imgSrc: 'https://placehold.jp/300x150.png',
+    username: 'noc7t',
+    title: '四角'
+  },
+  {
+    imgSrc: 'https://placehold.jp/450x300.png',
+    username: 'nokhnaton',
+    title: '四角'
+  },
+  {
+    imgSrc: 'https://placehold.jp/250x250.png',
+    username: 'noc7t',
+    title: '四角'
+  },
+  {
+    imgSrc: 'https://placehold.jp/1600x1200.png',
+    username: 'nokhnaton',
+    title: '四角'
+  }
+]
 </script>
 
 <template>
-  <div :class="$style.about">
-    <document-card
-      img-src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-      username="noc7t"
-      title="ハンバーガー"
-    ></document-card>
-    <document-card
-      img-src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
-      username="nokhnaton"
-      title="よくわからん鳥"
-    ></document-card>
+  <div v-masonry="1" transition-duration="0.3s" item-selector=".item" column-width="450">
+    <div
+      v-masonry-tile
+      class="item"
+      v-for="(item, index) in documents"
+      :key="index"
+      :class="$style.card"
+    >
+      <document-card
+        :img-src="item.imgSrc"
+        :username="item.username"
+        :title="item.title"
+      ></document-card>
+    </div>
   </div>
 </template>
 
 <style module>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    /* display: flex; */
-    align-items: center;
-  }
+.card {
+  width: 450px;
+  padding: 10px;
 }
 </style>
