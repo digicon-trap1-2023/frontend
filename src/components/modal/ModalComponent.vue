c4d425a1b433224c1986d2e53a7b49ad1946e676
nokhnaton <sage_of_forest@icloud.com>
2023-09-26 10:22:19 +0900

    modalの作成

---
+++ src/components/modal/ModalComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ElDialog } from 'element-plus'
+import { ref } from 'vue'
+
+// const props = defineProps<{
+//   // onClose: () => void
+// }>()
+</script>
+
+<template>
+  <ElDialog>
+    <slot />
+  </ElDialog>
+</template>
+
+<style module>
+.modalOutside {
+  position: fixed;
+  background-color: black;
+  opacity: 0.4;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import ModalComponent from '@/components/modal/ModalComponent.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <ModalComponent>
+      <div>aaaaaa</div>
+    </ModalComponent>
   </main>
 </template>
