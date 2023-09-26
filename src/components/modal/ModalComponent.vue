10f6120e06898d65981f2f56de53832397ce0331
nokhnaton <sage_of_forest@icloud.com>
2023-09-26 12:18:51 +0900

    modalの作成

--- src/components/modal/ModalComponent.vue
+++ src/components/modal/ModalComponent.vue
@@ -1,22 +1,39 @@
 <script setup lang="ts">
 import { ElDialog } from 'element-plus'
-import { ref } from 'vue'
 
-// const props = defineProps<{
-//   // onClose: () => void
-// }>()
+const props = defineProps<{
+  src: string
+  userId: string
+  userName: string
+}>()
 </script>
 
 <template>
-  <ElDialog>
-    <slot />
+  <ElDialog append-to-body lock-scroll :class="$style.modal" top="5vh">
+    <img src="https://placehold.jp/1000x1000.png" :class="$style.img" />
   </ElDialog>
 </template>
 
 <style module>
-.modalOutside {
-  position: fixed;
-  background-color: black;
-  opacity: 0.4;
+.modal {
+  max-width: 90vw;
+  width: fit-content;
+  height: 90vh;
+  margin-bottom: 0;
+  overscroll-behavior: contain;
+}
+.modal > header {
+  padding: 0;
+}
+.modal > div {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+}
+.img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  display: block;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import TheWelcome from '../components/TheWelcome.vue'
 import ModalComponent from '@/components/modal/ModalComponent.vue'
+
+const visible = ref(false)
 </script>
 
 <template>
   <main>
     <TheWelcome />
-    <ModalComponent>
-      <div>aaaaaa</div>
+    <button @click="visible = true">button</button>
+    <ModalComponent
+      v-model="visible"
+      :src="'https://placehold.jp/1000x1000.png'"
+      :user-id="'aaaa'"
+      :user-name="'noc7t'"
+    >
     </ModalComponent>
   </main>
 </template>
+
+<style module></style>
