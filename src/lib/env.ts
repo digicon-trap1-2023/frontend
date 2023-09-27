2e29c014fdc3a75d75e616a7bec6d9f79896be90
mehm8128 <mehm8128@gmail.com>
2023-09-27 21:35:17 +0900

    環境変数名修正

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,6 @@
 export const getApiOrigin = () => {
-  if (import.meta.env.NEXT_PUBLIC_ORIGIN !== undefined) {
-    return import.meta.env.NEXT_PUBLIC_ORIGIN
+  if (import.meta.env.VITE_PUBLIC_ORIGIN !== undefined) {
+    return import.meta.env.VITE_PUBLIC_ORIGIN
   }
   return 'http://localhost:8000'
 }
