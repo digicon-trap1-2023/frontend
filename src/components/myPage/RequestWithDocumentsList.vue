00d74392b50d92c86f81eaa6c256c8be710e071b
mehm8128 <mehm8128@gmail.com>
2023-09-27 21:27:32 +0900

    モーダル修正

--- src/components/myPage/RequestWithDocumentsList.vue
+++ src/components/myPage/RequestWithDocumentsList.vue
@@ -2,6 +2,8 @@
 import type { RequestWithDocuments } from '@/clients/request/types'
 import { ElButton, ElCollapse, ElCollapseItem } from 'element-plus'
 import { ref } from 'vue'
+import { ZoomIn } from '@element-plus/icons-vue'
+import DocumentModal from '@/components/modal/DocumentModal.vue'
 
 interface Props {
   requests: RequestWithDocuments[]
@@ -13,32 +15,58 @@ const activeRequestNames = ref<string[]>([])
 const handleChangeActiveRequest = (val: string[]) => {
   activeRequestNames.value = val
 }
+
+const isModalOpen = ref(false)
+const currentModalDocumentId = ref<string | null>(null)
+
+const handleSelectCurrentDocument = (id: string) => {
+  currentModalDocumentId.value = id
+  isModalOpen.value = true
+}
 </script>
 
 <template>
-  <el-collapse v-model="activeRequestNames" @change="handleChangeActiveRequest">
-    <el-collapse-item :name="request.id" v-for="request in requests" :key="request.id">
-      <template #title>
-        <p :class="$style.title">{{ request.title }}</p>
-      </template>
-      <ul>
-        <li v-for="document in request.documents" :key="document.id">
-          <router-link :to="`/documents?documentId=${document.id}`">
-            <el-button type="primary" link :class="$style.button">
-              {{ document.title }}
-            </el-button>
-          </router-link>
-        </li>
-      </ul>
-    </el-collapse-item>
-  </el-collapse>
+  <div>
+    <el-collapse v-model="activeRequestNames" @change="handleChangeActiveRequest">
+      <el-collapse-item :name="request.id" v-for="request in requests" :key="request.id">
+        <template #title>
+          <p :class="$style.requestTitle">{{ request.title }}</p>
+        </template>
+        <ul>
+          <li :class="$style.li" v-for="document in request.documents" :key="document.id">
+            <p :class="$style.documentTitle">{{ document.title }}</p>
+            <el-button
+              circle
+              :class="$style.button"
+              @click="handleSelectCurrentDocument(document.id)"
+              :icon="ZoomIn"
+            />
+          </li>
+        </ul>
+      </el-collapse-item>
+    </el-collapse>
+    <document-modal
+      @closed="() => (currentModalDocumentId = null)"
+      v-if="isModalOpen && currentModalDocumentId"
+      v-model="isModalOpen"
+      :document-id="currentModalDocumentId"
+    />
+  </div>
 </template>
 
 <style module>
 .button {
   font-size: 16px;
 }
-.title {
+.requestTitle {
+  font-size: 16px;
+}
+.li {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+.documentTitle {
   font-size: 16px;
 }
 </style>
