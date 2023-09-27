e5951af105c5633a36d24348489f9f66daebf2d1
mehm8128 <mehm8128@gmail.com>
2023-09-27 15:35:21 +0900

    ビューワーへリンクを貼った

--- src/components/myPage/ReferecedList.vue
+++ src/components/myPage/ReferecedList.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import type { Document } from '@/clients/document/types'
+import { useMeStore } from '@/stores/me'
+import { storeToRefs } from 'pinia'
 
 interface Props {
   documents: Document[]
 }
 
 defineProps<Props>()
+
+const meStore = useMeStore()
+const { role } = storeToRefs(meStore)
 </script>
 
 <template>
@@ -13,9 +18,14 @@ defineProps<Props>()
     <li v-for="document in documents" :key="document.id">
       <div>
         <router-link :to="`/documents?documentId=${document.id}`">{{ document.title }}</router-link>
-        <ul>
+        <ul v-if="role === 'reader'">
           <li v-for="writer in document.referenced_users" :key="writer">
             {{ writer }}
+          </li>
+        </ul>
+        <ul v-else>
+          <li>
+            <router-link to="/book">ビューワーへ</router-link>
           </li>
         </ul>
       </div>
